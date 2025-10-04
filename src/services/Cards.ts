7e7c22f786987476f1ff1f64bd142e538fc36419
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-04 14:13:53 +0200

    eliminate code warnings

--- src/services/Cards.ts
+++ src/services/Cards.ts
@@ -149,7 +149,9 @@ const cards = [
 ]
 
 const cardsMap = new Map<number,Card>()
-cards.forEach(card => cardsMap.set(card.id, card))
+for (const card of cards) {
+  cardsMap.set(card.id, card)
+}
 
 export default {
 
